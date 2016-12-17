63a45cc7fd4dd8736d4e7a12b73c922f5587912b
Drake Bennion <drakebennion@gmail.com>
2016-12-16 18:36:11 -0700

    centering horizontally and moving the quote down 100px.

--- quotab.js
+++ quotab.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
-	var quote = document.querySelector('h3');
-	var author = document.querySelector('p');
-	//container.textContent = 'Well, here we are!';
-
+	var author = document.getElementById('author');
+	var quote = document.getElementById('quote');
 	var request = new XMLHttpRequest();
+	var response;
 	var url  = 'http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en';
+
 	request.open('GET', url);
 
 	request.onload = function () {
-		var response = JSON.parse(request.response);
+		response = JSON.parse(request.response);
 		quote.textContent = response.quoteText;
 		author.textContent = response.quoteAuthor;
 	};
