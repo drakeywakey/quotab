74db1ea25b94c89223e1ac10c592ce0b99df7b2b
Drake Bennion <drakebennion@gmail.com>
2016-12-16 18:23:55 -0700

    quotes being loaded -- now, for some style

--- quotab.js
+++ quotab.js
@@ -1,4 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
-	var container = document.querySelector('section');
-	container.textContent = 'Well, here we are!';
+	var quote = document.querySelector('h3');
+	var author = document.querySelector('p');
+	//container.textContent = 'Well, here we are!';
+
+	var request = new XMLHttpRequest();
+	var url  = 'http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en';
+	request.open('GET', url);
+
+	request.onload = function () {
+		var response = JSON.parse(request.response);
+		quote.textContent = response.quoteText;
+		author.textContent = response.quoteAuthor;
+	};
+
+	request.send();
 });
