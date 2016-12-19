7535780531f22f9f7267b30c08a04a745ba7c775
Drake Bennion <drakebennion@gmail.com>
2016-12-18 18:36:03 -0700

    small refactor to slightly abstract the xmlHttpRequest

--- quotab.js
+++ quotab.js
@@ -5,14 +5,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function getNewQuote() {
 	var author = document.getElementById('author');
+	var callback;
 	var quote = document.getElementById('quote');
-	var request = new XMLHttpRequest();
-	var response;
 	var url  = 'http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en';
 
-	request.open('GET', url);
-
-	request.onload = function () {
+	callback = function (request) {
 		try {
 			// remove all \ from the string -- I can't imagine the quote will need the \,
 			// and their choice of \' messes with the JSON.parse.
@@ -25,13 +22,12 @@ function getNewQuote() {
 		}
 	};
 
-	request.send();
+	makeRequest(url, callback);
 }
 
 function getWordOfTheDay() {
+	var callback;
 	var lastRequestTime = localStorage.getItem('lastWordOfDayRequest');
-	var request = new XMLHttpRequest();
-	var response;
 	// this isn't my api key, but they just left it hanging around on their website so...
 	// I should only be calling this once a day anyway.
 	var url = 'http://api.wordnik.com/v4/words.json/wordOfTheDay?api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5';
@@ -46,9 +42,7 @@ function getWordOfTheDay() {
 		wordDefElement.textContent = localStorage.getItem('wordDef');
 	}
 	else {
-		request.open('GET', url);
-
-		request.onload = function () {
+		callback = function (request) {
 			response = JSON.parse(request.response);
 			word = response.word;
 			wordDef = response.definitions[0].text;
@@ -62,6 +56,17 @@ function getWordOfTheDay() {
 			wordDef.textContent = wordDef;
 		};
 
-		request.send();
+		makeRequest(url, callback);
 	}
+}
+
+function makeRequest(url, xhrCallback) {
+	var request = new XMLHttpRequest();
+	var response;
+
+	request.open('GET', url);
+
+	request.onload = xhrCallback.bind(this, request);
+
+	request.send();
 }
